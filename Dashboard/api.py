b4a5d4129096a6d9efb7bf2b3eb9fe7e61202c7b
Manika <manikamittal2898>
2020-07-22 14:30:08 +0530

    Adding updated code of DasBoard

--- Dashboard/api.py
+++ Dashboard/api.py
@@ -1,15 +1,28 @@
-from flask import Flask, render_template, request
+# from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response,Response, jsonify, redirect
+# app = Flask(__name__)
+# from flask_cors import CORS, cross_origin
 from datetime import datetime
 import time
-
+# import dateutil.parser as parser
 
 app = Flask(__name__)
+# cors = CORS(app)
+# app.config['CORS_HEADERS'] = 'Content-Type'
 
+# @app.after_request
+# def after_request(response):
+#     response.headers.add('Access-Control-Allow-Origin', '*')
+#     response.headers.add('Access-Control-Allow-Credentials', 'true')
+#     response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+#     response.headers.add('Access-Control-Allow-Headers', 'Accept')
+#     response.headers.add('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+#     return response
 
 
 @app.route('/dashboard')
 def showDashboard() :
-    time.sleep(5)
+    time.sleep(1)
     data = {}
     if len(request.args) == 0:
         data = {'st' : '', 'et' : '', 'app_i' : [], 'app_x' :[]}
@@ -24,13 +37,19 @@ def showDashboard() :
     et = str(et[0])[:19]
     st1 = datetime.strptime(st, "%Y-%m-%dT%H:%M:%S")
     et1 = datetime.strptime(et, "%Y-%m-%dT%H:%M:%S")
+    print(st1)
+    print(parser.parse(str(st1)).isoformat())
     mon = "0" + str(st1.month) if st1.month < 10 else str(st1.month)
     day = "0" + str(st1.day) if st1.day < 10 else str(st1.day)
-    st1 = mon + "/" + day + "/" + str(st1.year) + " " + str(st1.hour) + ":" + str(st1.minute)
+    hour = "0" + str(st1.hour) if st1.hour < 10 else str(st1.hour)
+    min = "0" + str(st1.minute) if st1.minute < 10 else str(st1.minute)
+    st1 = mon + "/" + day + "/" + str(st1.year) + " " + str(hour) + ":" + str(min)
 
     mon = "0" + str(et1.month) if et1.month < 10 else str(et1.month)
     day = "0" + str(et1.day) if et1.day < 10 else str(et1.day)
-    et1 = mon + "/" + day + "/" + str(et1.year) + " " + str(et1.hour) + ":" + str(et1.minute)
+    hour = "0" + str(et1.hour) if et1.hour < 10 else str(et1.hour)
+    min = "0" + str(et1.minute) if et1.minute < 10 else str(et1.minute)
+    et1 = mon + "/" + day + "/" + str(et1.year) + " " + str(hour) + ":" + str(min)
     data = {'st' : st1, 'et' : et1, 'app_i' : ls_cf_apps_include, 'app_x' :ls_cf_apps_exclude}
     return render_template('dashboard.html', data=data)
 
@@ -43,4 +62,4 @@ def showNewDashboard() :
 
 
 if __name__ == "__main__":
-    app.run(debug=True,port=5015)
+    app.run(debug=True,port=5025)
--- Dashboard/static/components/index.js
+++ Dashboard/static/components/index.js
@@ -15,7 +15,10 @@ let exceptionMap = new Map()
 let colors = ["rgba(110,57,42,0.3)", "rgba(36,2,114,0.6)", "rgba(178,250,226,0.9)", "rgba(171,253,56,0.0)", "rgba(177,147,20,0.2)", "rgba(125,156,38,0.7)", "rgba(158,132,222,0.1)", "rgba(28,118,29,0.0)", "rgba(149,201,179,0.4)", "rgba(247,207,79,0.1)", "rgba(161,49,201,0.7)", "rgba(58,129,36,0.2)", "rgba(79,17,145,0.8)", "rgba(188,10,148,0.8)", "rgba(189,249,89,0.5)", "rgba(58,61,29,0.9)", "rgba(105,153,47,0.4)", "rgba(208,160,83,0.1)", "rgba(168,53,120,0.2)", "rgba(28,117,191,0.5)", "rgba(90,221,14,0.7)", "rgba(179,65,166,1.0)", "rgba(143,94,103,0.7)", "rgba(240,91,227,0.0)", "rgba(242,104,76,0.7)", "rgba(37,171,140,0.1)", "rgba(147,128,23,0.5)", "rgba(140,151,63,0.1)", "rgba(69,229,77,0.8)", "rgba(250,88,156,0.8)", "rgba(140,177,200,1.0)", "rgba(170,64,91,0.2)", "rgba(154,229,167,0.1)", "rgba(143,244,233,0.2)", "rgba(194,212,154,0.3)", "rgba(9,6,65,0.4)", "rgba(242,246,60,0.9)", "rgba(91,40,90,0.4)", "rgba(193,4,184,0.7)", "rgba(158,248,193,0.4)", "rgba(18,130,243,0.1)", "rgba(32,242,131,0.3)", "rgba(156,244,14,0.6)", "rgba(51,117,240,0.3)", "rgba(116,141,17,0.3)", "rgba(217,226,68,0.8)", "rgba(253,250,4,0.6)", "rgba(125,81,6,0.1)", "rgba(173,82,255,0.8)", "rgba(128,190,192,0.3)", "rgba(191,169,149,0.5)", "rgba(203,84,210,0.8)", "rgba(195,176,219,0.5)", "rgba(81,170,221,1.0)", "rgba(222,254,176,0.7)", "rgba(21,122,89,0.4)", "rgba(193,24,66,0.7)", "rgba(134,150,160,0.9)", "rgba(88,40,193,0.4)", "rgba(187,116,134,1.0)", "rgba(218,165,216,0.8)", "rgba(246,149,95,0.1)", "rgba(75,136,92,0.0)", "rgba(131,56,93,0.2)", "rgba(177,121,137,0.0)", "rgba(252,91,96,0.2)", "rgba(79,71,224,0.8)", "rgba(207,21,224,0.2)", "rgba(242,24,94,0.8)", "rgba(76,170,16,0.5)", "rgba(94,248,130,0.8)", "rgba(132,40,160,0.5)", "rgba(171,203,50,0.0)", "rgba(141,234,19,0.5)", "rgba(82,240,147,0.0)", "rgba(216,87,252,0.0)", "rgba(32,147,181,0.8)", "rgba(152,154,64,0.8)", "rgba(74,15,252,0.4)", "rgba(105,116,213,0.6)", "rgba(86,77,50,0.5)", "rgba(216,239,184,0.0)", "rgba(210,190,127,0.6)", "rgba(156,20,70,0.9)", "rgba(64,138,152,0.1)", "rgba(209,127,252,0.1)", "rgba(194,38,118,0.3)", "rgba(236,15,166,0.9)", "rgba(98,59,59,0.1)", "rgba(8,165,42,0.1)", "rgba(175,200,47,0.2)", "rgba(114,19,6,0.7)", "rgba(215,219,253,0.6)", "rgba(109,18,189,0.8)", "rgba(250,180,20,1.0)", "rgba(203,114,133,1.0)", "rgba(229,53,134,0.3)", "rgba(149,189,210,0.9)", "rgba(20,243,227,0.2)", "rgba(220,55,220,0.9)"]
 
 
-
+function random_rgba() {
+    var o = Math.round, r = Math.random, s = 255;
+    return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+}
 
 
 
@@ -44,7 +47,8 @@ document.getElementById('visual').addEventListener('click', function (e) {
             appList.forEach(function (element) {
                 labelz.push(element)
                 datas.push(appsMap.get(element))
-                colorz.push(colors[i])
+                // colorz.push(colors[i])
+                colorz.push(random_rgba())
                 i = i + 1;
             });
 
@@ -54,7 +58,8 @@ document.getElementById('visual').addEventListener('click', function (e) {
             exceptionList.forEach(function (element) {
                 labelz.push(element)
                 datas.push(exceptionMap.get(element))
-                colorz.push(colors[i])
+                // colorz.push(colors[i])
+                colorz.push(random_rgba())
                 i = i + 1;
             });
 
@@ -268,15 +273,20 @@ function fetchDetails(lk) {
             let tableContent = document.getElementById('contents')
             if (res.length > 0) {
 
-                tableContent.innerHTML = `<thead class="thead-dark">
+                tableContent.innerHTML = `<thead class="thead-dark" style='margin-top:${val+5}px'>
     <tr>
-      <th scope="col">timestamp8601</th>
-      <th scope="col">cf_app_name</th>
-      <th scope="col">Exception Name</th>
-      <th scope="col">Error Message</th>
-      <th scope="col">Exception Details</th>
+      <th scope="col" style='font-size: 15px'>Timestamp </th>
+      <th scope="col" style='font-size: 15px'>App Name</th>
+      <th scope="col" style='font-size: 15px'>Exception Name</th>
+      <th scope="col" style='font-size: 15px'>Error Message</th>
+      <th scope="col" style='font-size: 15px'>Exception Details</th>
     </tr>
   </thead><tbody>`
+//   th {
+//     position: sticky;
+//     top: 50px;  /* 0px if you don't have a navbar, but something is required */
+//     background: white;
+//   }
                 appsMap.clear()
                 exceptionMap.clear()
                 appsSize = 0
@@ -374,36 +384,36 @@ document.getElementById('query').addEventListener('input', function (e) {
                 tr[i].style.display = "none"
             }
         }
-        if (isFound == false) {
-            td = tr[i].getElementsByTagName('td')[3]
-            console.log(td)
-            if (td) {
-                textVal = td.textContent || td.innerText;
-                if (textVal.toUpperCase().indexOf(filter) > -1) {
-                    tr[i].style.display = "";
-                    count = count + 1
-                    isFound = true;
+        // if (isFound == false) {
+        //     td = tr[i].getElementsByTagName('td')[3]
+        //     console.log(td)
+        //     if (td) {
+        //         textVal = td.textContent || td.innerText;
+        //         if (textVal.toUpperCase().indexOf(filter) > -1) {
+        //             tr[i].style.display = "";
+        //             count = count + 1
+        //             isFound = true;
 
-                } else {
-                    tr[i].style.display = "none"
-                }
-            }
-        }
-        if (isFound == false) {
-            td = tr[i].getElementsByTagName('td')[4]
-            console.log(td)
-            if (td) {
-                textVal = td.textContent || td.innerText;
-                if (textVal.toUpperCase().indexOf(filter) > -1) {
-                    tr[i].style.display = "";
-                    count = count + 1
-                    isFound = true;
+        //         } else {
+        //             tr[i].style.display = "none"
+        //         }
+        //     }
+        // }
+        // if (isFound == false) {
+        //     td = tr[i].getElementsByTagName('td')[4]
+        //     console.log(td)
+        //     if (td) {
+        //         textVal = td.textContent || td.innerText;
+        //         if (textVal.toUpperCase().indexOf(filter) > -1) {
+        //             tr[i].style.display = "";
+        //             count = count + 1
+        //             isFound = true;
 
-                } else {
-                    tr[i].style.display = "none"
-                }
-            }
-        }
+        //         } else {
+        //             tr[i].style.display = "none"
+        //         }
+        //     }
+        // }
     }
     document.getElementById('count').innerHTML = 'Total Count - ' + count
 });
